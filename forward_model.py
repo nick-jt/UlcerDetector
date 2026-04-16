56254bf76967dac2ae9be9b6ea80cff7ee00a930
Nick Tricard <ntricard@mit.edu>
2026-04-16 09:40:16 -0600

    Ulcer appears – diffusion fails

--- forward_model.py
+++ forward_model.py
@@ -1,9 +1,37 @@
 import torch
+from torch.autograd import forward_ad
 import matplotlib
 import matplotlib.pyplot as plt
 import os
+import tqdm
 
-def rhs(t, y, dydt, args):
+# Setting GPU
+dtype = torch.float
+device = torch.device("cuda")
+
+@torch.compile
+def laplacian(V,dx,dy):
+    lap = torch.zeros_like(V)
+    lap[1:-1, 1:-1] = (V[2:, 1:-1] - 2 * V[1:-1, 1:-1] + V[:-2, 1:-1]) / dx**2.0 + \
+      (V[1:-1, 2:] - 2 * V[1:-1, 1:-1] + V[1:-1,:-2]) / dy**2.0
+
+    # X boundaries
+    lap[0, 1:-1] = 2*(V[1, 1:-1]- V[0, 1:-1]) / dx**2 + (V[0, 2:] - 2 * V[0, 1:-1] + V[0,:-2]) / dy**2.0
+    lap[-1, 1:-1] = 2*(-V[-1, 1:-1]+ V[-2, 1:-1]) / dx**2 + (V[-1, 2:] - 2 * V[-1, 1:-1] + V[-1,:-2]) / dy**2.0
+
+    # Y boundaries
+    lap[1:-1, 0] = (V[2:, 0] - 2 * V[1:-1, 0] + V[:-2, 0]) / dx**2.0 + 2*(V[1:-1, 1]- V[1:-1, 0]) / dy**2 
+    lap[1:-1, -1] = (V[2:, -1] - 2 * V[1:-1, -1] + V[:-2, -1]) / dx**2.0+ 2*(-V[1:-1,-1]+ V[ 1:-1,-2]) / dy**2
+
+    # Corners
+    lap[0,0] = 2 * (V[1, 0] - V[0, 0]) / dx**2.0 + 2 * (V[0, 1] - V[0, 0]) / dy **2
+    lap[-1,-1] = 2 * (V[-2, -1] - V[-1, -1]) / dx**2.0 + 2 * (V[-1, -2] - V[-1, -1]) / dy **2
+    lap[0,-1] = 2 * (V[1, -1] - V[0, -1]) / dx**2.0 + 2 * (V[0, -2] - V[0, -1]) / dy **2
+    lap[-1,0] = 2 * (V[-2, 0] - V[-1, 0]) / dx**2.0 + 2 * (V[-1, 1] - V[-1, 0]) / dy **2
+    return lap
+
+
+def rhs(t, y, dydt, args, params):
     """2-D reaction-diffusion model of skin
     
     dT(x,y,t)/dt = D_T(x,y) * laplacian(T) + f(T, U)
@@ -12,46 +40,59 @@ def rhs(t, y, dydt, args):
     for T = temperature and U = ulcer value
         x,y = positions
         t = time
-        D_T(x,y) = diffusion coefficient of temperature
-        f(T, U) = reaction term for temperature
-        g(T, U) = reaction term for ulcer
+        q_src(x,y,t) = reaction term for temperature
     
     Args:
         t: time
         y: state vector [N, M, 2]
-            y[:, :, 0] = temperature
-            y[:, :, 1] = ulcer value
+            y = temperature
     
     Returns:
         rhs: right-hand side of the ODE
     """
-    T, U = y[:,:,0], y[:,:,1]
-    D_T = 1000.0
-    f = lambda T, U: 0.0
-    g = lambda T, U: 0.0
+    T = y
+
     dx = args['dx']
     dy = args['dy']
+    D_T = args['D_T']
+    h = args['h']
+    T_amb = args['T_amb']
 
-    def laplacian(V,dx,dy):
-        lap = torch.zeros_like(V)
-        lap[1:-1, 1:-1] = (V[2:, 1:-1] - 2 * V[1:-1, 1:-1] + V[:-2, 1:-1]) / dx**2.0 + (V[1:-1, 2:] - 2 * V[1:-1, 1:-1] + V[1:-1,:-2]) / dy**2.0
-
-        # X boundaries
-        lap[0, 1:-1] = 2*(V[1, 1:-1]- V[0, 1:-1]) / dx**2 + (V[0, 2:] - 2 * V[0, 1:-1] + V[0,:-2]) / dy**2.0
-        lap[-1, 1:-1] = 2*(-V[-1, 1:-1]+ V[-2, 1:-1]) / dx**2 + (V[-1, 2:] - 2 * V[-1, 1:-1] + V[-1,:-2]) / dy**2.0
-        # Y boundaries
-        lap[1:-1, 0] = (V[2:, 0] - 2 * V[1:-1, 0] + V[:-2, 0]) / dx**2.0 + 2*(V[1:-1, 1]- V[1:-1, 0]) / dy**2 
-        lap[1:-1, -1] = (V[2:, -1] - 2 * V[1:-1, -1] + V[:-2, -1]) / dx**2.0+ 2*(-V[1:-1,-1]+ V[ 1:-1,-2]) / dy**2 
-        # Corners
-        lap[0,0] = # TODO
-        return lap
+    q_src = params['q_src']
 
     laplacian_T = laplacian(T, dx,dy)
-
-    dydt[:,:,0] = D_T * laplacian_T + f(T, U)
-    dydt[:,:,1] = g(T, U)
-
+    
+    dydt = D_T * laplacian_T + q_src - h * (T - T_amb)
+    
     return dydt
+
+
+def forward_solve(dt, y0, args,params):
+
+    N = args['N']
+    M = args['M']
+    plot_gt_interval = args['plot_gt_interval']
+
+    t0 = 0.0
+    tfinal = args['tfinal']
+    nsteps = int((tfinal - t0)/dt)
+    dydt = torch.ones(N,M,2, dtype=dtype, device=device)
+    fig,ax = plt.subplots(1,1,dpi=250)
+
+    y = y0
+    print("\nBeginning timesteps")
+    for step in tqdm.tqdm(range(nsteps), desc="Time stepping"):      
+      t = t0 + step * dt                                  
+      dy_dt = rhs(t, y, dydt, args, params)
+      y = y + dt * dy_dt
+
+
+      # plotting
+      if step % plot_gt_interval == 0:
+        im0 = ax.imshow(y[:,:].detach().cpu().numpy())#, vmin=300, vmax=350)
+        if t == dt:
+          fig.colorbar(im0, ax=ax)
+        plt.savefig(f"ulcer_{t}.png")
 
 
 
@@ -59,47 +100,44 @@ def main():
 
     # Output directori
     #os.mkdir("data", exist_ok=True)
-    plot_gt = True
+    plot_gt_interval = 1000
+    print_arguments = True
 
-
-
-    # Run forward model
+    # Static args
     N, M = 100, 100
-    t = 0.0
-    dt = 0.1
-    args = {'dx': 1.0/N, 'dy':1.0/M}
-    y = torch.ones(N,M,2) 
-    y[:,:,0] *= 310.0 # average human body temp
-    y[:,:,1] *= 0.0 # no ulcers anywhere
-    
-    # Ground truth ulcer
-    cx, cy, dx,dy  = 50,50, 4,4
-    y[cx-dx:cx+dx, cy-dy:cy+dy, 0] = 320
-    y[cx-dx:cx+dx, cy-dy:cy+dy, 1] = 1
+    args = {
+      'dx': 1.0/N, 
+      'dy': 1.0/M,
+      'tfinal': 1.0,
+      'N': 100,
+      'M': 100,
+      'D_T': 10.0,
+      'T_amb': 320,
+      'h': 0.0,
+      'plot_gt_interval': plot_gt_interval
+    }
+    dt = min(args['dx']**2, args['dy']**2) / (2 * args['D_T']) # Stability requirement: dt <= dx^2 / (4 * D)
 
+    if print_arguments:
+        print("Arguments:")
+        for key, value in args.items():
+            print(f"{key}: {value}")
+        print(f"dt: {dt}")
 
-    dydt = torch.ones(N,M,2)
-    fig,ax = plt.subplots(1,2,dpi=250)
+    # Initial conditions
+    y0 = torch.ones(N,M, dtype=dtype, device=device)  * 310.0 # Human body temp
+    cx, cy, dx,dy  = 50,50, 4,4 # Adding new ulcer
+    q_src = torch.zeros((N,M), dtype=dtype, device=device)
+    q_src[cx-dx:cx+dx, cy-dy:cy+dy] = 10.0
 
-    while t < 1.0:
-        dy_dt = rhs(t, y, dydt,args)
-        y = y + dt * dy_dt
-        t += dt
+    # Reverse mode
+    q_src = torch.nn.Parameter(q_src) 
+    params = {'q_src': q_src}
 
-        # plotting
-        if plot_gt:
-          im0 = ax[0].imshow(y[:,:,0].detach().cpu().numpy(), vmin=300, vmax=350)
-          im1 = ax[1].imshow(y[:,:,1].detach().cpu().numpy(), vmin=0, vmax=1)
-          if t == dt:
-            fig.colorbar(im0, ax=ax[0])
-            fig.colorbar(im1, ax=ax[1])
-          plt.savefig(f"ulcer_{t}.png")
-
+    print("\nSolving ground truth...")
+    y = forward_solve(dt, y0, args,params)
 
     return y
-
-
-
 
 
 if __name__ == "__main__":
